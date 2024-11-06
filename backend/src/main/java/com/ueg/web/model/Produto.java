2550d60d787f4eb9bf82a6f3875eb4716c33d3c9
heuber.lima <heuber.lima@gmail.com>
2024-11-06 20:31:01 -0300

    Primeiro commit - adicionando backend, app mobile e app web

---
+++ backend/src/main/java/com/ueg/web/model/Produto.java
@@ -0,0 +1,61 @@
+package com.ueg.web.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produto")
+public class Produto {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="quantidade")
+	private int quantidade;
+	
+	
+	
+	public Produto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Produto(Long id, String nome, int quantidade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.quantidade = quantidade;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+
+}
